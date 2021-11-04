0a0f35b4e1288ce18ec369429862fab16f7d0882
Projnajit <89357118+Projnajit@users.noreply.github.com>
2021-11-04 19:09:00 +0600

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ import yaml
 import mysql.connector as mysql
 from mysql.connector.constants import ClientFlag
 from sqlalchemy import create_engine
+from db_connection import get_database_connection
 # import smtplib
  
 import streamlit as st
@@ -38,7 +39,7 @@ st.set_page_config(
 )
  #database localhost connection
  #@st.cache()
-
+cursor, db = get_database_connection()
 
 ## all using functions start
 sub_id=''
@@ -406,28 +407,9 @@ def driver():
 
 
 
-
-with open('credintials.yml', 'r') as f:
-    credintials = yaml.load(f, Loader=yaml.FullLoader)
-    db_credintials = credintials['db']
-    system_pass = credintials['system_pass']['admin']
-    # email_sender = credintials['email_sender']
-
-
-def get_database_connection():
-    db = mysql.connect(host = db_credintials['host'],
-                      user = db_credintials['user'],
-                      passwd = db_credintials['passwd'],
-                      database = db_credintials['database'],
-                      auth_plugin= db_credintials['auth_plugin'])
-    cursor = db.cursor()
-
-    return cursor, db
-
-
 ## all using functions end
 
-#cursor, db = get_database_connection()
+
  
  
 cursor.execute('''CREATE TABLE register (
